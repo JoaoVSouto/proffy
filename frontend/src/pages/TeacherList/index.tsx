265eab452e387aa0d33d3845ff9b300343474b84
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-03 20:33:38 -0300

    [FRONTEND] Styles study page header

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Header, TopBarContainer, HeaderContent } from './styles';
+
+import logoImg from '../../assets/images/logo.svg';
+
+import backIcon from '../../assets/images/icons/back.svg';
 
 const TeacherList: React.FC = () => {
-  return <h1>teacher list</h1>;
+  return (
+    <Container className="container">
+      <Header>
+        <TopBarContainer>
+          <Link to="/">
+            <img src={backIcon} alt="Voltar" />
+          </Link>
+
+          <img src={logoImg} alt="Proffy" />
+        </TopBarContainer>
+
+        <HeaderContent>
+          <strong>Estes são os proffys disponíveis.</strong>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
 };
 
 export default TeacherList;
