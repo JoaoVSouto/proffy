9b59bcee774d0051de310204b9a7eb6c6248727e
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-03 19:00:07 -0300

    [FRONTEND] Styles landing page for desktop

--- frontend/src/pages/Landing/styles.ts
+++ frontend/src/pages/Landing/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { up } from 'styled-breakpoints';
 
 export const PageLanding = styled.div`
   width: 100vw;
@@ -12,9 +13,21 @@ export const PageLanding = styled.div`
   background-color: var(--color-primary);
 `;
 
-export const PageLandingContent = styled.div``;
+export const PageLandingContent = styled.div`
+  ${up('lgDesktop')} {
+    max-width: 1200px !important;
+
+    display: grid;
+    grid-template-rows: 350px 1fr;
+    grid-template-columns: 2fr 1fr 1fr;
+    grid-template-areas:
+      'logo hero hero'
+      'buttons buttons total';
+  }
+`;
 
 export const LogoContainer = styled.div`
+  grid-area: logo;
   text-align: center;
   margin-bottom: 3.2rem;
 
@@ -28,13 +41,35 @@ export const LogoContainer = styled.div`
     line-height: 4.6rem;
     margin-top: 0.8rem;
   }
+
+  ${up('lgDesktop')} {
+    align-self: center;
+    margin: 0;
+
+    img {
+      height: 100%;
+      float: left;
+    }
+
+    h2 {
+      width: 80%;
+      text-align: initial;
+      font-size: 3.6rem;
+    }
+  }
 `;
 
 export const HeroImage = styled.img`
+  grid-area: hero;
   width: 100%;
+
+  ${up('lgDesktop')} {
+    justify-self: end;
+  }
 `;
 
 export const ButtonsContainer = styled.div`
+  grid-area: buttons;
   display: flex;
   justify-content: center;
   margin: 3.2rem 0;
@@ -82,9 +117,18 @@ export const ButtonsContainer = styled.div`
       margin-right: 2rem;
     }
   }
+
+  ${up('lgDesktop')} {
+    justify-content: flex-start;
+
+    a {
+      font-size: 2.4rem;
+    }
+  }
 `;
 
 export const TotalConnections = styled.span`
+  grid-area: total;
   font-size: 1.4rem;
 
   display: flex;
@@ -93,5 +137,9 @@ export const TotalConnections = styled.span`
 
   img {
     margin-left: 0.8rem;
+  }
+
+  ${up('lgDesktop')} {
+    justify-self: end;
   }
 `;
