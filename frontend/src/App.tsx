46fb8541c8ffb76f9b968c62987eb6494a0c94f0
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-03 17:45:58 -0300

    [FRONTEND] Creates global styles file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <h1>hello world</h1>
+
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
