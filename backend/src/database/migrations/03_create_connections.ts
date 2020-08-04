e370caa01a2958216236d6d960d8dd6886ac0596
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-04 12:05:31 -0300

    [BACKEND] Fixes created_at field default value on connections table

--- backend/src/database/migrations/03_create_connections.ts
+++ backend/src/database/migrations/03_create_connections.ts
@@ -4,7 +4,10 @@ export async function up(knex: Knex): Promise<SchemaBuilder> {
   return knex.schema.createTable('connections', table => {
     table.increments('id').primary();
 
-    table.timestamp('created_at').defaultTo('now()').notNullable();
+    table
+      .timestamp('created_at')
+      .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+      .notNullable();
 
     table
       .integer('user_id')
