d684df5c945586646c6733e020b2fd67adf97a29
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-04 10:01:52 -0300

    [BACKEND] Creates migration for creating classes schedule table

---
+++ backend/src/database/migrations/02_create_class_schedule.ts
@@ -0,0 +1,23 @@
+import Knex, { SchemaBuilder } from 'knex';
+
+export async function up(knex: Knex): Promise<SchemaBuilder> {
+  return knex.schema.createTable('classes_schedule', table => {
+    table.increments('id').primary();
+
+    table.integer('week_day').notNullable();
+    table.integer('from').notNullable();
+    table.integer('to').notNullable();
+
+    table
+      .integer('class_id')
+      .notNullable()
+      .references('id')
+      .inTable('classes')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex): Promise<SchemaBuilder> {
+  return knex.schema.dropTable('classes_schedule');
+}
