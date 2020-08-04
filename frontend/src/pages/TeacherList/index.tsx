4d4346d3ae955361d067ef0ee871356c4a8c6d92
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-03 21:58:45 -0300

    [FRONTEND] Styles TeacherItem for mobile

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
-import { Container, Form, InputBlock, Label, Input } from './styles';
+import {
+  Container,
+  Form,
+  InputBlock,
+  Label,
+  Input,
+  Main,
+  TeacherItem,
+} from './styles';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 const TeacherList: React.FC = () => {
   return (
@@ -25,6 +35,42 @@ const TeacherList: React.FC = () => {
           </InputBlock>
         </Form>
       </PageHeader>
+
+      <Main>
+        <TeacherItem>
+          <header>
+            <img
+              src="https://api.adorable.io/avatars/face/eyes5/nose7/mouth7/6633cc"
+              alt="User 1"
+            />
+
+            <div>
+              <strong>John Doe</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br />
+            <br />
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das
+            pessoas através de experiências. Mais de 200.000 pessoas já passaram
+            por uma das minhas explosões.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 20,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+          </footer>
+        </TeacherItem>
+      </Main>
     </Container>
   );
 };
--- frontend/src/pages/TeacherList/styles.ts
+++ frontend/src/pages/TeacherList/styles.ts
@@ -73,3 +73,93 @@ export const Input = styled.input`
   font-size: 1.6rem;
   font-family: 'Archivo', sans-serif;
 `;
+
+export const Main = styled.main`
+  margin: 3.2rem auto;
+  width: 90%;
+`;
+
+export const TeacherItem = styled.article`
+  background-color: var(--color-box-base);
+  border: 1px solid var(--color-line-in-white);
+  border-radius: 0.8rem;
+  margin-top: 2.4rem;
+  overflow: hidden;
+
+  header {
+    padding: 3.2rem 2rem;
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 8rem;
+      height: 8rem;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 2.4rem;
+
+      strong {
+        font-family: 'Archivo', sans-serif;
+        font-weight: 700;
+        font-size: 2.4rem;
+        display: block;
+        color: var(--color-text-title);
+      }
+
+      span {
+        font-size: 1.6rem;
+        display: block;
+        margin-top: 0.4rem;
+      }
+    }
+  }
+
+  > p {
+    padding: 0 2rem;
+    font-size: 1.6rem;
+    line-height: 2.8rem;
+  }
+
+  footer {
+    margin-top: 3.2rem;
+    padding: 3.2rem 2rem;
+    background-color: var(--color-box-footer);
+    border-top: 1px solid var(--color-line-in-white);
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    p strong {
+      color: var(--color-primary);
+      font-size: 1.6rem;
+      display: block;
+    }
+
+    button {
+      width: 20rem;
+      height: 5.6rem;
+      background-color: var(--color-secundary);
+      color: var(--color-button-text);
+      border: 0;
+      border-radius: 0.8rem;
+      cursor: pointer;
+      font-family: 'Archivo', sans-serif;
+      font-size: 1.4rem;
+      font-weight: 700;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+
+      transition: background-color 0.2s;
+
+      &:hover,
+      &:focus {
+        background-color: var(--color-secundary-dark);
+      }
+    }
+  }
+`;
