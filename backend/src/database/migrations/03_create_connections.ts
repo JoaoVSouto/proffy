c21b3b89e0a9110d480a0186bb80291448c35abd
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-04 10:09:21 -0300

    [BACKEND] Creates migration for creating connections table

---
+++ backend/src/database/migrations/03_create_connections.ts
@@ -0,0 +1,21 @@
+import Knex, { SchemaBuilder } from 'knex';
+
+export async function up(knex: Knex): Promise<SchemaBuilder> {
+  return knex.schema.createTable('connections', table => {
+    table.increments('id').primary();
+
+    table.timestamp('created_at').defaultTo('now()').notNullable();
+
+    table
+      .integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex): Promise<SchemaBuilder> {
+  return knex.schema.dropTable('connections');
+}
