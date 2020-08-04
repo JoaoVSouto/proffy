2b6486b68ecd926f2aa8c9e10494dee582537778
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-04 09:54:30 -0300

    [BACKEND] Creates migration for creating classes table

---
+++ backend/src/database/migrations/01_create_classes.ts
@@ -0,0 +1,21 @@
+import Knex, { SchemaBuilder } from 'knex';
+
+export async function up(knex: Knex): Promise<SchemaBuilder> {
+  return knex.schema.createTable('classes', table => {
+    table.increments('id').primary();
+    table.string('subject').notNullable();
+    table.decimal('cost').notNullable();
+
+    table
+      .integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex): Promise<SchemaBuilder> {
+  return knex.schema.dropTable('classes');
+}
