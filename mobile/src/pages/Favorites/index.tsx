cd8bfdb4ff1f1b29e1488f4c3076a7597ad3f460
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-06 19:19:47 -0300

    [MOBILE] Adds header on favorites tab

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
 import { Container } from './styles';
 
 const Favorites: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <PageHeader title="Meus proffys Favoritos" />
+    </Container>
+  );
 };
 
 export default Favorites;
