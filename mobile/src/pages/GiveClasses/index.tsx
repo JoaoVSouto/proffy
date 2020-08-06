3506e452bd0e32af4838e0f15ec5dbf0d68baf1c
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-06 12:11:22 -0300

    [MOBILE] Styles GiveClasses page and creates navigate handler to go back

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import {
+  Container,
+  ImageBackground,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
 
 const GiveClasses: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  const handleNavigateBack = (): void => {
+    goBack();
+  };
+
   return (
     <Container>
-      <Text>Give classes</Text>
+      <ImageBackground resizeMode="contain" source={giveClassesBgImage}>
+        <Title>Quer ser um Proffy?</Title>
+        <Description>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Description>
+      </ImageBackground>
+
+      <OkButton onPress={handleNavigateBack}>
+        <OkButtonText>Tudo bem</OkButtonText>
+      </OkButton>
     </Container>
   );
 };
