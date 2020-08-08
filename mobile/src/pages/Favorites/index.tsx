5d16bc7e895d7a090f39750a61f4e8b76782f79c
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-08 14:05:41 -0300

    [MOBILE] Fills favorites page with favorited teachers

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem, { TeacherItemList } from '../../components/TeacherItem';
+import TeacherItem, {
+  TeacherItemList,
+  ITeacher,
+} from '../../components/TeacherItem';
 
-import { Container } from './styles';
+import { Container, NotFound } from './styles';
 
 const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState<ITeacher[]>([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      let isActive = true;
+
+      const loadFavorites = async (): Promise<void> => {
+        const favoritesData = await AsyncStorage.getItem('favorites');
+
+        if (favoritesData) {
+          const favoritedTeachers = JSON.parse(favoritesData);
+
+          if (isActive) {
+            setFavorites(favoritedTeachers);
+          }
+        }
+      };
+
+      loadFavorites();
+
+      return () => {
+        isActive = false;
+      };
+    }, [])
+  );
+
   return (
     <Container>
       <PageHeader title="Meus proffys Favoritos" />
 
+      {favorites.length === 0 && (
+        <NotFound>Você ainda não possui nenhum proffy favorito!</NotFound>
+      )}
+
       <TeacherItemList>
-        <TeacherItem />
+        {favorites.map(favorite => (
+          <TeacherItem key={favorite.id} teacher={favorite} favorited />
+        ))}
       </TeacherItemList>
     </Container>
   );
--- mobile/src/pages/Favorites/styles.ts
+++ mobile/src/pages/Favorites/styles.ts
@@ -4,3 +4,10 @@ export const Container = styled.View`
   flex: 1;
   background-color: #f0f0f7;
 `;
+
+export const NotFound = styled.Text`
+  color: #6a6180;
+  margin-top: 32px;
+  font-size: 16px;
+  text-align: center;
+`;
